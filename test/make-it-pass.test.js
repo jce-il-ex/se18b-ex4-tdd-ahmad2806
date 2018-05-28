c7bd7a321061be7c2d58438de831fd2b5a32eb28
ahmad2806 <ahmadhashem2806@gmail.com>
2018-05-28 17:50:36 +0300

    GREEN: sub function was added and tested

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -4,8 +4,14 @@ function sum (a, b) {
     return a+b;
   // your code to make the test pass goes here ...
 }
-
+function sub (a, b) {
+    return a-b;
+}
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+test('sum should return the subtraction of two numbers', function (t) {
+  t.equal(4, sub(6, 2));
   t.end();
 });
